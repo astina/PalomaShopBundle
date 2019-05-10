f492ec42ca1e0b5b41723fdca5bdb9d2375249e5
pkraeutli <pkraeutli@astina.ch>
2019-05-10 14:24:20 +0200

    PALO-745 mobile header and nav WIP

--- Resources/assets/js/app.js
+++ Resources/assets/js/app.js
@@ -1,8 +1,39 @@
 require('../css/paloma.scss');
 
+// jQuery
+import jQuery from 'jquery';
 import Vue from 'vue';
 
 import PalomaProductList from './PalomaProductList';
+
+(function($) {
+
+    const $categoryNav = $('.category-nav');
+    $categoryNav.find('.category-nav__burger').click((e) => {
+        e.preventDefault();
+        $categoryNav.toggleClass('is-active');
+    });
+
+    $categoryNav.find('.category-nav__menu-wrapper').click((e) => {
+        if (e.target === e.currentTarget) {
+            e.preventDefault();
+            $categoryNav.toggleClass('is-active');
+        }
+    });
+
+    $(window).on('scroll', () => {
+        const scrolled = $(window).scrollTop() > 80;
+        if (scrolled) {
+            $('body').addClass('is-scrolled');
+        } else {
+            $('body').removeClass('is-scrolled');
+        }
+    })
+
+})(jQuery);
+
+
+// Vue
 
 // TODO create plugin
 Vue.prototype.$trans = function(key) {
