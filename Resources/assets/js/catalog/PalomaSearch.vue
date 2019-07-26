631ce7d62ce544d8df423920d37f9c77ed7084fe
pkraeutli <pkraeutli@astina.ch>
2019-07-26 17:46:31 +0200

    PALO-745 quick search WIP

--- Resources/assets/js/catalog/PalomaSearch.vue
+++ Resources/assets/js/catalog/PalomaSearch.vue
@@ -1,10 +1,37 @@
 <template>
-    <div>
-        <a :href="searchUrl" class="button is-white">
+    <div class="quick-search">
+        <a @click.prevent="toggleForm()" class="button is-white quick-search__button" href="">
             <span class="icon">
                 <i class="fal fa-search"></i>
             </span>
         </a>
+        <div class="quick-search__form-wrapper" :class="{'quick-search__form-wrapper--active': showForm}">
+            <div class="quick-search__form">
+                <div class="container">
+
+                    <form method="get" :action="searchUrl">
+
+                        <div class="field has-addons">
+                            <div class="control is-expanded">
+                                <input v-if="showForm"
+                                       v-focus
+                                       class="input is-large" type="search" name="query"
+                                       value=""
+                                       :placeholder="$trans('search.placeholder')">
+                            </div>
+                            <div class="control">
+                                <button type="submit" class="button is-large">
+                                    <span class="icon">
+                                        <i class="far fa-search"></i>
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -17,7 +44,14 @@
 
         data() {
             return {
-                searchUrl: paloma.router.resolve('catalog_search')
+                searchUrl: paloma.router.resolve('catalog_search'),
+                showForm: false
+            }
+        },
+
+        methods: {
+            toggleForm() {
+                this.showForm = !this.showForm;
             }
         }
     }
