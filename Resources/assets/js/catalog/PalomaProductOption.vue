fa089f61f87e680787fb4d33f00dc7c878dc538b
pkraeutli <pkraeutli@astina.ch>
2019-09-10 16:46:02 +0200

    PALO-745 product variants: unavailable variants css

--- Resources/assets/js/catalog/PalomaProductOption.vue
+++ Resources/assets/js/catalog/PalomaProductOption.vue
@@ -4,7 +4,11 @@
         <h3 v-if="option.option !== '_variant'" class="product-option__title">{{ option.label }}</h3>
 
         <div v-for="value in values" :key="value.value" class="product-option__value"
-            :class="{'product-option__value--selected': value.value === selected, 'product-option__value--variant': value.variant}"
+            :class="{
+                    'product-option__value--selected': value.value === selected,
+                    'product-option__value--variant': value.variant,
+                    'product-option__value--disabled': !value.available
+                }"
             @click.prevent="selectValue(value)">
             <div class="product-option__value-control control">
                 <label class="radio">
@@ -33,9 +37,17 @@
         },
 
         data() {
+
+            const values = this.option.values.map(value => {
+
+                value.available = value.variant && value.variant.availability.available;
+
+                return value;
+            });
+
             return {
                 selected: this.option.selectedValue,
-                values: this.option.values
+                values: values
             }
         },
 
